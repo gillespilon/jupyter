7e2cf4ae9a1b25e744372915ffb7f0b95c999584
Gilles Pilon <gilles.pilon@gillespilon.ca>
2017-07-09 12:36:30 -0500

    Replace code with proper use of pandas and matplotlib
    - There is an error in the second chunk of code; the graph should be
    total and median, not all lines.

--- kpis/kpis.py
+++ kpis/kpis.py
@@ -8,51 +8,51 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-commits = pd.read_csv('kpis.csv')
+commits = pd.read_csv('kpis.csv', parse_dates=True, index_col='date')
 commits['total']= commits.private + commits.support + commits.jupyter + commits.tableau
+commits['total_median'] = commits['total'].median()
 
-total_median = commits['total'].median()
-total_xmin = commits['datenum'].min()
-total_xmax = commits['datenum'].max()
+title = 'Key Performance Indicator'
+subtitle = 'Git Commits'
+ylabel = 'Commits'
+xlabel = 'Date'
 
-graphtitle = 'Key Performance Indicator'
-graphsubtitle = 'Git Commits'
-graphylabel = 'Commits'
-graphxlabel = 'Day'
+ax1 = plt.subplot(111)
+commits.plot.line(y='private', ax=ax1, color='red', \
+                  marker='o', markersize='3', label='Private')
+commits.plot.line(y='support', ax=ax1, color='blue', \
+                  marker='o', markersize='3', label='Support')
+commits.plot.line(y='jupyter', ax=ax1, color='green', \
+                  marker='o', markersize='3', label='Jupyter')
+commits.plot.line(y='tableau', ax=ax1, color='yellow', \
+                  marker='o', markersize='3', label='Tableau')
+ax1.spines['right'].set_color('none')
+ax1.spines['top'].set_color('none')
+ax1.tick_params(direction='out')
+ax1.xaxis.set_ticks_position('bottom')
+ax1.yaxis.set_ticks_position('left')
+ax1.set_title(title + '\n' + subtitle)
+ax1.set_ylabel(ylabel)
+ax1.set_xlabel(xlabel)
+ax1.axis('auto')
+ax1.legend(frameon=False)
+ax1.figure.savefig('kpi_commits.svg', format='svg')
+ax1.figure.savefig('kpi_commits.pdf', format='pdf')
 
-ax = plt.subplot(111)
-ax.spines['right'].set_color('none')
-ax.spines['top'].set_color('none')
-ax.tick_params(direction='out')
-ax.xaxis.set_ticks_position('bottom')
-ax.yaxis.set_ticks_position('left')
-plt.plot(commits.datenum, commits.private, marker='o', color="red", linewidth=1.0, markersize=3, linestyle="-", label="private/", )
-plt.plot(commits.datenum, commits.support, marker='o', color="blue", linewidth=1.0, markersize=3, linestyle="-", label="support/")
-for i, txt in enumerate( commits.supportnote ):
-    ax.annotate( txt, ( commits.datenum[i] + .5, commits.support[i] + .5 ), color="blue" )
-plt.plot(commits.datenum, commits.jupyter, marker='o', color="green", linewidth=1.0, markersize=3, linestyle="-", label="jupyter/")
-plt.plot(commits.datenum, commits.tableau, marker='o', color="yellow", linewidth=1.0, markersize=3, linestyle="-", label="tableau/")
-plt.title(graphtitle + '\n' + graphsubtitle)
-plt.ylabel(graphylabel)
-plt.xlabel(graphxlabel)
-ax.legend(loc='upper left', scatterpoints=1, frameon=False)
-plt.savefig('kpi_commits.svg', format='svg')
-plt.savefig('kpi_commits.pdf', format='pdf')
-plt.show()
-
-
-ax = plt.subplot(111)
-ax.spines['right'].set_color('none')
-ax.spines['top'].set_color('none')
-ax.tick_params(direction='out')
-ax.xaxis.set_ticks_position('bottom')
-ax.yaxis.set_ticks_position('left')
-plt.plot(commits.datenum, commits.total, marker='o', color="blue", linewidth=1.0, markersize=3, linestyle="-", label="total", )
-plt.plot([total_xmin, total_xmax], [total_median, total_median], color="red",          linewidth=1.0,          linestyle="-", label="median", )
-plt.title(graphtitle + '\n' + graphsubtitle)
-plt.ylabel(graphylabel)
-plt.xlabel(graphxlabel)
-plt.savefig('kpi_commits_total.svg', format='svg')
-plt.savefig('kpi_commits_total.pdf', format='pdf')
-plt.show()
-
+ax2 = plt.subplot(111)
+commits.plot.line(y='total', ax=ax2, color='blue', \
+                  marker='o', markersize='3', label='Total commits')
+commits.plot.line(y='total_median', ax=ax2, color="red", \
+         linewidth=1.0, linestyle="-", label='Median')
+ax2.spines['right'].set_color('none')
+ax2.spines['top'].set_color('none')
+ax2.tick_params(direction='out')
+ax2.xaxis.set_ticks_position('bottom')
+ax2.yaxis.set_ticks_position('left')
+ax2.set_title(title + '\n' + subtitle)
+ax2.set_ylabel(ylabel)
+ax2.set_xlabel(xlabel)
+ax2.axis('auto')
+ax2.legend(frameon=False)
+ax2.figure.savefig('kpi_commits_total.svg', format='svg')
+ax2.figure.savefig('kpi_commits_total.pdf', format='pdf')
