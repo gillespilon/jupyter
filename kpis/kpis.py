fb8cf0ede3608bc8fe4bc0a95db17fc7c10f9de4
Gilles Pilon <gilles.pilon@gillespilon.ca>
2017-07-12 10:59:06 -0500

    Use simpler approach by letting pandas color the lines, for the 1st of 2 graphs
    - Copied the code for graph 1 from kpis.ipynb.
    - Used one line of code to plot four graphs, not four lines of code.

--- kpis/kpis.py
+++ kpis/kpis.py
@@ -8,8 +8,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-commits = pd.read_csv('kpis.csv', parse_dates=True, index_col='date')
-commits['total']= commits.private + commits.support + commits.jupyter + commits.tableau
+commits = pd.read_csv('kpis.csv', parse_dates=True, index_col='Date')
+commits['total']= commits.Private + commits.Support + commits.Jupyter + commits.Tableau
 commits['total_median'] = commits['total'].median()
 
 title = 'Key Performance Indicator'
@@ -17,28 +17,32 @@ subtitle = 'Git Commits'
 ylabel = 'Commits'
 xlabel = 'Date'
 
+# Create a graph of "individual commits v. date"
+#
+# Create a single subplot
 ax1 = plt.subplot(111)
-commits.plot.line(y='private', ax=ax1, color='red', \
-                  marker='o', markersize='3', label='Private')
-commits.plot.line(y='support', ax=ax1, color='blue', \
-                  marker='o', markersize='3', label='Support')
-commits.plot.line(y='jupyter', ax=ax1, color='green', \
-                  marker='o', markersize='3', label='Jupyter')
-commits.plot.line(y='tableau', ax=ax1, color='yellow', \
-                  marker='o', markersize='3', label='Tableau')
+# Plot "total commits v. date"
+commits[['Private', 'Support', 'Jupyter', 'Tableau']] \
+    .plot.line(legend=True, ax=ax1, marker='o', markersize=3).axis('auto')
+# Remove the top and right spines
 ax1.spines['right'].set_color('none')
 ax1.spines['top'].set_color('none')
+# Place the ticks outside the axes
 ax1.tick_params(direction='out')
 ax1.xaxis.set_ticks_position('bottom')
 ax1.yaxis.set_ticks_position('left')
 ax1.set_title(title + '\n' + subtitle)
+# Add the Y axis label
 ax1.set_ylabel(ylabel)
+# Add the X axis label
 ax1.set_xlabel(xlabel)
-ax1.axis('auto')
+# Remove box around legend
 ax1.legend(frameon=False)
+# Save the graph as svg and pdf.
 ax1.figure.savefig('kpi_commits.svg', format='svg')
 ax1.figure.savefig('kpi_commits.pdf', format='pdf')
 
+# Close the current figure window
 plt.close()
 
 ax2 = plt.subplot(111)
